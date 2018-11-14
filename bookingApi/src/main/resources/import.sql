4343ec1fcc804fc142649dd35f7c3e52a323ff97
rw1202 <rob120216822@gmail.com>
2018-11-14 16:39:28 +0000

    included import for database (untested)

---
+++ bookingApi/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO Booking (bookingId, dateAndTime, userId) VALUES (1, '28/11/2018 18:00', 1)
+INSERT INTO Booking (bookingId, dateAndTime, userId) VALUES (1, '30/11/2018 17:00', 1)
+
+
+INSERT INTO User  ( userName, userId) VALUES ('Steve Kerr', 1)
+INSERT INTO User  ( userName, userId) VALUES ('Michael Jordan', 2)
+INSERT INTO User  ( userName, userId) VALUES ('Scottie Pippen', 3)
+INSERT INTO User  ( userName, userId) VALUES ('Dennis Rodman', 5)
+INSERT INTO User  ( userName, userId) VALUES ('Toni Kukoc', 4)
